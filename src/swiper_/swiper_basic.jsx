a5d28fe3984de8d7b98832a42d4ec16adc8ed04b
ellie <gyeongjin.hong@dailyh.org>
2023-05-30 17:28:04 +0900

    + swiper 수정

---
+++ src/swiper_/swiper_basic.jsx
@@ -0,0 +1,39 @@
+import "../style.scss";
+import React from "react";
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/scss";
+import "swiper/scss/pagination";
+import "swiper/scss/navigation";
+
+// import required modules
+import { Pagination, Navigation } from "swiper";
+
+function Swiper_basic() {
+  return (
+    <div>
+      <Swiper
+        pagination={{
+          type: "fraction",
+        }}
+        navigation={true}
+        modules={[Pagination, Navigation]}
+        className="first_Swiper"
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
+    </div>
+  );
+}
+export default Swiper_basic;
